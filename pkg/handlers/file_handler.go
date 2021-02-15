8e384ba689c71a53bf0b6b1e836c2d945c38c3bb
GabbyMrH <gabbymrh@gmail.com>
2021-02-15 18:35:02 +0800

    feat:新增文件助手函数

---
+++ pkg/handlers/file_handler.go
@@ -0,0 +1,11 @@
+package handlers
+
+// 获取文件的扩展名
+func Ext(path string) string {
+	for i := len(path) - 1; i >= 0 && path[i] != '/'; i-- {
+		if path[i] == '.' {
+			return path[i:]
+		}
+	}
+	return ""
+}
